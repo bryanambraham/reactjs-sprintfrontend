26d27582d902940da0441bdd34f9d89209ef3193
bryanambraham <bryanambraham@gmail.com>
2025-03-28 14:23:10 +0700

    3/28/25

--- src/components/CargoSearch.jsx
+++ src/components/CargoSearch.jsx
@@ -78,7 +78,7 @@ const CargoSearch = () => {
   }
 
   return (
-    <div className="w-full flex justify-center z-50" id="cargo-search">
+    <div className="w-full flex justify-center my-10" id="cargo-search">
       <div className="max-w-3xl w-full bg-white rounded-lg shadow-md px-4">
         {/* Header */}
         <div className="mt-4 bg-secondary text-text-primary text-center py-4 rounded-t-lg">
